9c210e91e0a4d0363a7d44d91b1f798c9aa2811f
javiersolan010 <javisolanossr2@gmail.com>
2024-11-07 12:21:35 -0500

    Proyecto-Taller5

--- Proyecto-Taller5/src/Ejercicio3_MayorDeTresNumeros.java
+++ Proyecto-Taller5/src/Ejercicio3_MayorDeTresNumeros.java
@@ -1,12 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/***
+ * Dados tres números, determinar cuál es el mayor. Si son iguales, mostrar un mensaje.
+ * @author Javier Solano
  */
-
-/**
- *
- * @author tatia
- */
+import java.util.Scanner;
 public class Ejercicio3_MayorDeTresNumeros {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner tcl = new Scanner(System.in);
+        System.out.println("DAME TRES NUMEROS: ");
+        a = tcl.nextInt();
+        b = tcl.nextInt();
+        c = tcl.nextInt();
+        System.out.println("EL MAYOR ES: " + (( a > b > c ) ? a : b : c));
+        /*mayor = (a>b>c) ? a : b : c ;
+        System.out.println("EL MAYOR ES: " + mayor); */
+       
+       
+        
+    }
+
+    
+            
+    
+}
+    }
     
 }
