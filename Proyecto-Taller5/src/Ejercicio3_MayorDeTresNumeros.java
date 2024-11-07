da3710b2e9a50436223c8144ee99ab8c2ada65cd
javiersolan010 <javisolanossr2@gmail.com>
2024-11-07 12:32:42 -0500

    Proyecto-Taller5

--- Proyecto-Taller5/src/Ejercicio3_MayorDeTresNumeros.java
+++ Proyecto-Taller5/src/Ejercicio3_MayorDeTresNumeros.java
@@ -7,22 +7,28 @@ public class Ejercicio3_MayorDeTresNumeros {
     public static void main(String[] args) {
         int a, b, c;
         Scanner tcl = new Scanner(System.in);
-        System.out.println("DAME TRES NUMEROS: ");
+        System.out.println("DAME EL PRIMER NUMERO: ");
         a = tcl.nextInt();
+        System.out.println("DAME EL SEGUNDO NUMERO: ");
         b = tcl.nextInt();
+        System.out.println("DAME EL TERCER NUMERO: ");
         c = tcl.nextInt();
-        System.out.println("EL MAYOR ES: " + (( a > b > c ) ? a : b : c));
-        /*mayor = (a>b>c) ? a : b : c ;
-        System.out.println("EL MAYOR ES: " + mayor); */
-       
-       
-        
+        if (a>b && a>c)
+            
+        System.out.println("EL MAYOR ES: " +  a);
+        else if (b>a && b>c)
+            System.out.println("EL MAYOR ES: " + b );
+        else if (c>a && c>b)
+            System.out.println("EL MAYOR ES: " + c);
+        else if (a==b && a==c)
+            System.out.println("LOS NUMEROS SON IGUALES");
+           
     }
 
     
             
     
 }
-    }
     
-}
+    
+
